4bde0c2415d8fdac214ce643f591696e61fa3186
TheVytch <edu.vytch@gmail.com>
2023-10-21 14:32:04 -0300

    Usando componentes bootstrap, adicionado cabeçalho e rodape, usando data-binding como exemplo

--- controle-de-livros-lido/src/app/app.module.ts
+++ controle-de-livros-lido/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ProcurarComponent } from './procurar/procurar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HeaderComponent,
+    ProcurarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ controle-de-livros-lido/src/app/procurar/procurar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-procurar',
+  templateUrl: './procurar.component.html',
+  styleUrls: ['./procurar.component.css']
+})
+export class ProcurarComponent {
+
+  message: string = '';
+  mostrarElementos: boolean = true;
+  inputValue: string = '';
+
+  changeMessage() {
+    this.message = this.inputValue;
+  }
+
+  changeVisibility(){
+    this.mostrarElementos = !this.mostrarElementos;
+  }
+
+  onInputChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const sanitizedValue = inputElement.value.replace(/[0-9]/g, '');
+    inputElement.value = sanitizedValue;
+    this.inputValue = sanitizedValue;
+  }
+}
